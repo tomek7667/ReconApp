74e6067b4bd85b3fb400ff7760cc9c93035f9a6a
tomek7667 <s184571@student.pg.edu.pl>
2022-04-15 14:15:21 +0200

    01: Challenges finished, added new challenge todo: While loading progress in index, take into account submitted flags

--- routes/01.js
+++ routes/01.js
@@ -8,15 +8,38 @@ router.get('/', function(req, res, next) {
   res.render('01', {result: defaultResult});
 });
 
+
+// Challenge handling
 router.get('/notepad', function (req, res) {
+  let notes = fs.readFileSync("./data/01/notes.txt").toString().replaceAll("\r", "").split("\n");
+  let flag = false;
+  for (let note of notes) {
+    if (note.includes("<script>") && note.includes("</script>") && !JSON.parse(fs.readFileSync("./data/01/tasks.json").toString())[1].finished) {
+      flag = rot13Cipher(JSON.parse(fs.readFileSync("./data/01/tasks.json").toString())[1].flag);
+    }
+  }
+
+  if (notes.length > 1 && !JSON.parse(fs.readFileSync("./data/01/tasks.json").toString())[0].finished) {
+    flag = rot13Cipher(JSON.parse(fs.readFileSync("./data/01/tasks.json").toString())[0].flag);
+  }
   if (req.cookies && !req.cookies.pageSeen) {
     res.cookie("pageSeen", true);
-    res.render('01/notepad.ejs', {result: {msg: "Witaj w moim prywatnym notatniku!", success: true}});
+    res.render('01/notepad.ejs', {result: {msg: "Witaj w moim prywatnym notatniku!", success: true, notes: notes, flag: flag}});
   } else {
-    res.render('01/notepad.ejs', {result: defaultResult});
+    res.render('01/notepad.ejs', {result: {msg: false, success: true, notes: notes, flag: flag}});
   }
 });
 
+router.post('/addNote', function (req, res) {
+  if (req.body && req.body.note) {
+    let escaped = req.body.note.replaceAll("<script>", "").replaceAll("</script>", "");
+    fs.appendFileSync("./data/01/notes.txt", escaped+"\n");
+    res.send({result: {msg: false, success: true, reload: true}});
+  }
+})
+
+
+// Files handling
 router.post('/clearTask', function (req, res) {
     try {
       let progressData = JSON.parse(fs.readFileSync('./data/progressData').toString());
